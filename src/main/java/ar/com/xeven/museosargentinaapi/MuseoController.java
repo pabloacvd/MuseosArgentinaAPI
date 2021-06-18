f0aa03fb38b9569466ff3936a690c818be37a372
pabloacvd <pablo@xeven.com.ar>
2021-06-17 22:02:29 -0300

    Initial commit

---
+++ src/main/java/ar/com/xeven/museosargentinaapi/MuseoController.java
@@ -0,0 +1,33 @@
+package ar.com.xeven.museosargentinaapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MuseoController {
+
+    @GetMapping
+    public List<Museo> listarMuseos(){
+
+        RestTemplate apiMuseos = new RestTemplate();
+
+        ListaMuseos listaMuseos = apiMuseos.getForObject("https://www.cultura.gob.ar/api/v2.0/museos/", ListaMuseos.class);
+
+        return listaMuseos.getResults();
+    }
+
+    @GetMapping("nombres")
+    public List<String> listarNombresMuseos(){
+        RestTemplate apiMuseos = new RestTemplate();
+
+        ListaMuseos listaMuseos = apiMuseos.getForObject("https://www.cultura.gob.ar/api/v2.0/museos/", ListaMuseos.class);
+
+        return listaMuseos.getResults().stream().map(museo -> museo.getNombre()).toList();
+    }
+
+}
---
+++ src/test/java/ar/com/xeven/museosargentinaapi/MuseosArgentinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.xeven.museosargentinaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseosArgentinaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
